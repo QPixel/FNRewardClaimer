d5532f83b24c35f5e506ac377b386034f3bd8ca6
Riley Smith <riley@rileysmith.me>
2021-03-28 22:53:51 -0700

    fixed auto claim issue

--- src/index.js
+++ src/index.js
@@ -34,9 +34,10 @@ client.on("message", (message) => {
 	if (!message.content.startsWith(prefix) || message.author.bot) return;
   const args = message.content.slice(prefix.length).split(/ +/);
   const commandName = args.shift().toLowerCase();
-	
+
   if (!client.commands.has(commandName)) return;
   const command = client.commands.get(commandName);
+	if (command.name === "AutoClaim") return;
   try {
     command.execute(message, args);
   } catch (error) {
